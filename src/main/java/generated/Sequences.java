478d84e99128e8c1ae45b325bc2686c8883b62e7
Rahul Chatterjee <rahul@dell-large>
2017-09-01 15:49:00 -0400

    My changes

--- src/main/java/generated/Sequences.java
+++ src/main/java/generated/Sequences.java
@@ -24,7 +24,7 @@ import org.jooq.impl.SequenceImpl;
 public class Sequences {
 
 	/**
-	 * The sequence <code>public.system_sequence_989c496c_533d_4515_9cb2_e1af42b53c0f</code>
+	 * The sequence <code>public.system_sequence_bc40e215_b807_4dcf_990b_df6d5ce0dad1</code>
 	 */
-	public static final Sequence<Long> SYSTEM_SEQUENCE_989C496C_533D_4515_9CB2_E1AF42B53C0F = new SequenceImpl<Long>("system_sequence_989c496c_533d_4515_9cb2_e1af42b53c0f", Public.PUBLIC, org.jooq.impl.SQLDataType.BIGINT);
+	public static final Sequence<Long> SYSTEM_SEQUENCE_BC40E215_B807_4DCF_990B_DF6D5CE0DAD1 = new SequenceImpl<Long>("system_sequence_bc40e215_b807_4dcf_990b_df6d5ce0dad1", Public.PUBLIC, org.jooq.impl.SQLDataType.BIGINT);
 }
