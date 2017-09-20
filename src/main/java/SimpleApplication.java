36bf22f341f57907c90eb7489275168a00d68cf9
Adam Fleming <adam@honestbuildings.com>
2017-09-20 19:43:11 -0400

    Add ReceiptImageController

--- src/main/java/SimpleApplication.java
+++ src/main/java/SimpleApplication.java
@@ -46,6 +46,6 @@ public class SimpleApplication extends Application<Configuration> {
         env.jersey().register(new ReceiptController(receiptDao, tagDao));
         env.jersey().register(new TagsController(tagDao));
         env.jersey().register(new NetidController());
-
+        env.jersey().register(new ReceiptImageController());
     }
 }
